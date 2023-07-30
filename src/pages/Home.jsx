2f2efdbb08b1a8ae620b885f7874e557347e21a5
luyenkieu22 <twonine569@gmail.com>
2023-07-30 10:52:18 +0700

    update link home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Home = ({products}) => {
-  console.log('Home data: ', products)
+const Home = ({ products }) => {
+  console.log("Home data: ", products);
   return (
     <div>
+      <h1>
+        <Link to={`/admin/product`}>Admin Products</Link>
+      </h1>
       {products.map((items, index) => {
         return (
           <div key={index + 1}>
-            <h3><Link to={`/detail/${items.id}`}>{items.name}</Link></h3>
+            <h3>
+              <Link to={`/detail/${items.id}`}>{items.name}</Link>
+            </h3>
             <p>{items.price}</p>
           </div>
-        )
-      })
-      }
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
